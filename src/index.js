eb947b8688e4806406bde9ffe1cdb8ff40af0cbb
Sivert <sivertbjorn@gmail.com>
2020-02-22 02:39:34 +0100

    All is setup, ready to code!!

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class Arbeid extends Component {
+  render() {
+    return (
+      <div>
+        <h1 align="center">Denne siden er under arbeid dessverre</h1>
+        <br />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Arbeid />, document.getElementById("root"));
