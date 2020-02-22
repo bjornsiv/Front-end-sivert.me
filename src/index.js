df48e2b3ed6ae53e2aa622e5c0c42af789045d5d
bjornsiv <55831515+bjornsiv@users.noreply.github.com>
2020-02-23 00:37:46 +0100

    Completed "work in progress" site and finally managed to get bootstrap working

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Arbeid extends Component {
   render() {
     return (
-      <div>
-        <h1 align="center">Denne siden er under arbeid dessverre</h1>
-        <br />
+      <div align="center" className="jumbotron jumbotron-fluid">
+        <h1 className="display-4">Denne siden er under arbeid dessverre</h1>
+        <p className="lead">Kos deg på reddit i mellomtiden 😅</p>
+        <hr className="my-4"></hr>
+        <p className="lead">
+          <img src="../assets/reddit_logo.png" width="48px" height="48px" />
+          <a className="btn btn-primary btn-lg" href="https://www.reddit.com" role="button">Reddit</a>
+        </p>
       </div>
     );
   }
