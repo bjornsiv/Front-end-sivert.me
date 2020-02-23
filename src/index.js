2debd6880bd0ff32b3e6e5695112e5304868b348
bjornsiv <55831515+bjornsiv@users.noreply.github.com>
2020-02-23 23:07:25 +0100

    Trying to get a landing page up and running

--- src/index.js
+++ src/index.js
@@ -2,20 +2,32 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-class Arbeid extends Component {
-  render() {
-    return (
-      <div align="center" className="jumbotron jumbotron-fluid">
-        <h1 className="display-4">Denne siden er under arbeid dessverre</h1>
-        <p className="lead">Kos deg på reddit i mellomtiden 😅</p>
-        <hr className="my-4"></hr>
-        <p className="lead">
-          <img src="https://www.redditinc.com/assets/images/site/reddit-logo.png" height="32px" width="32px"/>
-          <a className="btn btn-primary btn-lg" href="https://www.reddit.com" role="button">Reddit</a>
-        </p>
-      </div>
-    );
+class Forside extends Component{
+  render(){
+    return(
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <a className="navbar-brand" href="#">Sivert.me</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item active">
+        <a className="nav-link" href="#">Hjem <span className="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="/nettside/">The Adventure of Cassi</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Arbeid</a>
+      </li>
+    </ul>
+
+    </div>
+  </nav>
+    )
   }
 }
 
-ReactDOM.render(<Arbeid />, document.getElementById("root"));
+ReactDOM.render(<Forside />, document.getElementById("root"));
