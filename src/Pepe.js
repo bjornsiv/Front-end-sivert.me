456ca8d549d4c69c5b46e6d260a548281a309414
bjornsiv <55831515+bjornsiv@users.noreply.github.com>
2020-02-26 01:08:59 +0100

    Small fix

--- src/Pepe.js
+++ src/Pepe.js
@@ -6,12 +6,12 @@ class Pepe extends Component{
       <div align="center" className="jumbotron jumbotron-fluid">
         <br/>
         <h1>Pepe</h1>
-        <img src='../Assets/pepe.png'></img>
-        <img src='../Assets/pepe1.gif'></img>
-        <img src='../Assets/pepe2.png'></img>
-        <img src='../Assets/pepe3.png'></img>
-        <img src='../Assets/pepe4.jpg'></img>
-        <img src='../Assets/pepe5.jpg'></img>
+        <img src='./Assets/pepe.png'></img>
+        <img src='./Assets/pepe1.gif'></img>
+        <img src='./Assets/pepe2.png'></img>
+        <img src='./Assets/pepe3.png'></img>
+        <img src='./Assets/pepe4.jpg'></img>
+        <img src='./Assets/pepe5.jpg'></img>
       </div>
     )
   }
