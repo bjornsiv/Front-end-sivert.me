b82aae0310c937f80fb714771845c40dc0a826fe
Sivert <sivertbjorn@gmail.com>
2021-12-14 20:23:24 +0100

    updated all packages

--- src/Links.js
+++ src/Links.js
@@ -9,13 +9,17 @@ class Links extends Component {
         <hr className="my-4"></hr>
         <table>
           <tbody>
-            <tr className="lead">
-              <th align="center">
-                <td><a href="/1Prosjekter/Bobler.html">Enkle bobler</a></td><br/>
-                <td><a href="/1Prosjekter/greenBottles.html">Green bottles</a></td><br/>
-                <td><a href="/1Prosjekter/Deling.html">Litt deling</a></td><br/>
-                <td><a href="/1Prosjekter/battleship.html">Battleships</a></td><br/>
-              </th>
+            <tr className="lead" align="center">
+                <td><a href="/1Prosjekter/Bobler.html">Enkle bobler</a></td>
+              </tr>
+              <tr className="lead" align="center">
+                <td><a href="/1Prosjekter/greenBottles.html">Green bottles</a></td>
+                </tr>
+              <tr className="lead" align="center">
+                <td><a href="/1Prosjekter/Deling.html">Litt deling</a></td>
+                </tr>
+              <tr className="lead" align="center">
+                <td><a href="/1Prosjekter/battleship.html">Battleships</a></td>
             </tr>
           </tbody>
         </table>
